3f47df4a9ff83daca9e6db358a4e9407bac89570
Drew2134 <a.winchell1896@gmail.com>
2022-12-03 21:04:58 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 require([
     //ArcGIS JS API
     "esri/config",
-    "esri/WebScene",
+    "esri/Map",
     "esri/layers/GeoJSONLayer",
-    "esri/views/SceneView",
+    "esri/views/MapView",
     "esri/symbols/WebStyleSymbol",
     "esri/smartMapping/renderers/location",
 
@@ -24,9 +24,18 @@ require([
     //Dojo
     "dojo/domReady!"
 
-], function(esriConfig, WebScene, GeoJSONLayer, SceneView, WebStyleSymbol, LocationRendererCreator, Home, Search, Collapse, Dropdown, CalciteMaps, CalciteMapArcGISSupport){
+], function(esriConfig, Map, GeoJSONLayer, MapView, WebStyleSymbol, LocationRendererCreator, Home, Search, Collapse, Dropdown, CalciteMaps, CalciteMapArcGISSupport){
 
 //esri agol api key
 esriConfig.apiKey = "AAPK81762376d6974634a978fa72c12fdfbdqcwchnbMurlNeJgk4ov0WxRZLEi9rayVxvBJeTGwyKw9Vy2_Azi6YAtY1QAlpkkm";
+
+const map = new Map({
+    basemap: "topo-vector"
+});
+
+const view = new MapView({
+    container: "viewDiv",
+    map: map
+});
 
 });
