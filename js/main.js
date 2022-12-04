87b1427851c6c5f41fe28aa58dd0f3f9ccf3769d
Drew2134 <a.winchell1896@gmail.com>
2022-12-04 15:31:18 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ require([
     //ArcGIS JS API
     "esri/config",
     "esri/Map",
-    "esri/layers/GeoJSONLayer",
+    "esri/layers/FeatureLayer",
     "esri/views/MapView",
     "esri/symbols/WebStyleSymbol",
     "esri/smartMapping/renderers/location",
@@ -24,13 +24,31 @@ require([
     //Dojo
     "dojo/domReady!"
 
-], function(esriConfig, Map, GeoJSONLayer, MapView, WebStyleSymbol, LocationRendererCreator, Home, Search, Collapse, Dropdown, CalciteMaps, CalciteMapArcGISSupport){
+], function(esriConfig, Map, FeatureLayer, MapView, WebStyleSymbol, LocationRendererCreator, Home, Search, Collapse, Dropdown, CalciteMaps, CalciteMapArcGISSupport){
 
 //esri agol api key
 esriConfig.apiKey = "AAPK81762376d6974634a978fa72c12fdfbdqcwchnbMurlNeJgk4ov0WxRZLEi9rayVxvBJeTGwyKw9Vy2_Azi6YAtY1QAlpkkm";
 
+const eventLayer = new FeatureLayer({
+    id: "weather_events",
+    url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/SITREP_DATA/FeatureServer/1",
+    minScale: 250000
+});
+
+let eventSymbol = {
+    type: "simple-point",
+    color: "red",
+    size: 4
+};
+
+eventLayer.renderer = {
+    type: "simple",
+    symbol: eventSymbol
+};
+
 const map = new Map({
-    basemap: "topo-vector"
+    basemap: "topo-vector",
+    layers: [eventLayer]
 });
 
 const view = new MapView({
